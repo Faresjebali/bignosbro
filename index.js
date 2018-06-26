d00c1764416e93cbf6c4dd6458d373a7a2164776
Faresjebali <38439810+Faresjebali@users.noreply.github.com>
2018-06-26 04:21:35 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+
+client.on("ready", () => {
+  // This event will run if the bot starts, and logs in, successfully.
+  client.user.setActivity('BigNoseBro Gang');
+});
+client.on('ready', () => {
+    setInterval(function(){
+        client.guilds.get('443776243385630723').roles.find('name', 'Rainbow').edit({color: 'RANDOM'})
+    },10000);
+});
+
+
+client.login('NDYwOTkyOTAwMDg0MjY5MDc4.DhM0oA.VkELyXdd-zvOw7ZYUKlWotTLcuI');
